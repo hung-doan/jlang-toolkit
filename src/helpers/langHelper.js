2cad4eea43f4ea8dbe5ff6d47a671df6d0684196
Hung Doan <hungdt@imt-soft.com>
2017-03-02 13:08:47 +0700

    remove unused code in langHelpe

--- src/helpers/langHelper.js
+++ src/helpers/langHelper.js
@@ -1,13 +1,6 @@
 var singleTranslate = require('google-translate-api');
 
-singleTranslate('Ik spreek Engels', {to: 'en'}).then(res => {
-    console.log(res.text);
-    //=> I speak English
-    console.log(res.from.language.iso);
-    //=> nl
-}).catch(err => {
-    console.error(err);
-});
+
 
 /**
  * options.from
